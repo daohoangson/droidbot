23cc47c22a7a7b6b921c00d9e1f3a3ec1510ad8a
Dao Hoang Son <daohoangson@gmail.com>
2024-10-23 09:13:00 +0700

    Add reveal trailing icon

--- app/src/main/java/com/daohoangson/droidbot/MainActivity.kt
+++ app/src/main/java/com/daohoangson/droidbot/MainActivity.kt
@@ -6,10 +6,17 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Lock
+import androidx.compose.material.icons.outlined.Lock
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.daohoangson.droidbot.ui.theme.DroidTakeOverTheme
@@ -34,6 +41,7 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun ApiKeyInput(modifier: Modifier = Modifier) {
     var apiKey by remember { mutableStateOf("") }
+    var reveal by remember { mutableStateOf(false) }
 
     Column(modifier = modifier) {
         OutlinedTextField(
@@ -41,6 +49,18 @@ fun ApiKeyInput(modifier: Modifier = Modifier) {
             label = { Text(stringResource(R.string.enter_api_key)) },
             modifier = Modifier.fillMaxWidth(),
             onValueChange = { apiKey = it },
+            keyboardOptions = KeyboardOptions(
+                keyboardType = KeyboardType.Password
+            ),
+            trailingIcon = {
+                IconButton(onClick = { reveal = !reveal }) {
+                    Icon(
+                        imageVector = if (reveal) Icons.Outlined.Lock else Icons.Filled.Lock,
+                        contentDescription = if (reveal) "Hide password" else "Show password"
+                    )
+                }
+            },
+            visualTransformation = if (reveal) VisualTransformation.None else PasswordVisualTransformation(),
         )
 
         Spacer(modifier = Modifier.height(16.dp))
